1952fb86e91bc72bc0a3928785ec0834a3cf197b
Wes Hinsley <w.hinsley@imperial.ac.uk>
2018-05-09 18:10:50 +0100

    Readme and more build.bat hacks

--- microbit-projects/build.bat
+++ microbit-projects/build.bat
@@ -6,10 +6,17 @@ rem particular cpp file; it seems hard-wired to main.cpp
 rem
 rem It seems quicker for me to solve it in a simple switch way
 rem than to figure out yotta, so here it is.
-del build\bbc-microbit-classic-gcc\source\*.hex
+
+rem Delete anything that potentially needs deleting from the
+rem source folder. Leave microepi.h, as everything needs it.
+
+del build\bbc-microbit-classic-gcc\source\*.hex >NUL
+del source\serialno.h >NUL
+del source\main.cpp >NUL
 
 IF EXIST "projects\%1\main.cpp" (
-  COPY "projects\%1\main.cpp" source >NUL
+  COPY "projects\%1\*.cpp" source >NUL
+  COPY "projects\%1\*.h" source >NUL
   yt build
 ) ELSE (
   ECHO projects\%1\main.cpp not found. Try....
