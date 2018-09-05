16f0b6ec119d0a5805f0a4e202271be94b06210b
Wes Hinsley <w.hinsley@imperial.ac.uk>
2018-09-05 18:37:02 +0100

    License text

--- bin/python-gui/epi_serial.py
+++ bin/python-gui/epi_serial.py
@@ -1,4 +1,33 @@
 '''
+epi_serial.py is part of the micro:bit epidemic project.
+It handles communication between the management GUI, and
+the master micro:bit via the serial port.
+
+The MIT License (MIT)
+
+Copyright (c) 2018 Wes Hinsley
+MRC Centre for Global Infectious Disease Analysis
+Department of Infectious Disease Epidemiology
+Imperial College London
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
 Created on 6 Jun 2018
 
 @author: Wes Hinsley
--- src/java-projects/slideshow/LangSupport.java
+++ src/java-projects/slideshow/LangSupport.java
@@ -1,5 +1,36 @@
-// Language Support. Wes, Jan 2017
-// First use: Barcode Epidemic V3, Wes, Jan 2017
+/*
+LangSupport.java is part of the micro:bit epidemic project.
+It is part of the MicroEpiSlideshow code, and allows all
+text in the slideshow to be looked up from an external 
+file, thus making future multi-language translations of
+the slideshow possible.
+
+The MIT License (MIT)
+
+Copyright (c) 2018 Wes Hinsley
+MRC Centre for Global Infectious Disease Analysis
+Department of Infectious Disease Epidemiology
+Imperial College London
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+*/
 
 import java.io.BufferedReader;
 import java.io.FileReader;
